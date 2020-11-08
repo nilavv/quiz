126049fd9715efa781ff2779a1c01a4878adea1c
nilavv <vaghasiya.nilav@gmail.com>
2020-11-08 17:23:16 -0600

    Basic files and modules added

---
+++ src/app/services/quiz.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizService {
+
+  constructor(private http: HttpClient) {
+    
+   }
+   get(url:string){
+     return this.http.get(url);
+   }
+}
